971b5f9592cc3e25f9d4306e33340e6a1be4dbc8
Vale <duh_online@yahoo.com>
2023-06-26 21:35:43 +0300

    first debugging exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+function testDebugging() {
+  let num1 = 10;
+  let num2 = 5;
+
+  // Add a breakpoint on the line below to pause execution
+  let result = num1 + num2;
+
+  console.log("The result is: " + result);
+}
