0656da46351caa80377e7d42ff23fda796c029ff
Adina Caniuca <caniucaadina@gmail.com>
2023-06-27 19:22:08 +0300

    Aditional modification / return explanation

--- script.js
+++ script.js
@@ -8,13 +8,17 @@ function testDebugging() {
   console.log("The result is: " + result);
 }
 
+
+let myResult = [];
 let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function myProblem(arr) {
   let arr2 = [];
   for (let i = 0; i < arr.length; i++) {
     arr2.push(arr[i] * 2);
   }
-  return arr2;
+  return arr2; // trebuie pus return la final, pentru a avea stocat rezultatul intr-o variabila ce poate apoi fi folosita si in afara functiei cu rezultatul in ea / daca folosesti return rezultatul functiei poate fi incarcat intr-un alt parametru;
+  
 }
 
-console.log(myProblem(arr1));
+myResult = myProblem(arr1);
+console.log(myResult); 
